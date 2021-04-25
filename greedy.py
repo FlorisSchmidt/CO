e1162ebd070a9d70c0d3e16148f347025fce2bde
floris.schmidt <floris.schmidt@hotmail.com>
2021-04-25 13:45:51 +0200

    Extracted common functions

--- greedy.py
+++ greedy.py
@@ -1,54 +1,14 @@
 from InstanceVerolog2019 import InstanceVerolog2019
 import classes
 import math
+import functions
 
 def solve(instance):
 
     solution = classes.Solution(instance.Days)
-
-    # --- FUNCTIONS NEEDED --- #
-
-    # return euclidan distance
-    def euclidean(xa,ya,xb,yb):
-        return math.ceil(math.sqrt(math.pow((xa-xb),2) + math.pow((ya-yb),2)))
-
-    # Insert new location in route. e.g. [1,2,1] -> [1,2,3,1]
-    def insert_in_route(location,route):
-        old_route = []
-        for item in route:
-            old_route.append(item)
-        old_route.insert(-1,location)
-        return old_route
-
-    # Convert a route list eg. [0,1,2,0] to a list of coordinates eg. [[0,0],[120,120],[320,456],[0,0]]
-    def route_to_coordinates(routelist):
-        coordinate_list = []
-        for loc in routelist:
-            coordinate_list.append([instance.Locations.get(loc)[0], instance.Locations.get(loc)[1]])
-        return coordinate_list
-
-    # Calculate distance needed to travel route
-    def calc_route_distance(routelist):
-        #print(routelist)
-        coordinate_list = route_to_coordinates(routelist)
-        #print(coordinate_list)
-        distance = 0
-        for i in range(len(coordinate_list)-1):
-            distance += euclidean(coordinate_list[i][0], coordinate_list[i][1],coordinate_list[i+1][0], coordinate_list[i+1][1])
-        #print(distance)
-        return distance
-
-    def get_used_capacity(truck, instance):
-        cap = 0
-        for request in truck.requests:
-            cap += instance.Machines[request.machineID - 1].size * request.amount
-        return cap
-
     requests_in_trucks = []
     requests_technicianed = []
     requests_allocated = []
-
-    # --- ALGORITHM --- #
 
     # --- TRUCKS ---#
     # Loop through each day
@@ -61,10 +21,10 @@ def solve(instance):
             # check if request has already been allocated or first day is later then today
             if request in requests_in_trucks or request.firstDay > day.dayNumber:
                 continue
-            current_cap = get_used_capacity(trucksUsed[currentTruck], instance)
+            current_cap = functions.get_used_capacity(trucksUsed[currentTruck], instance)
             machine_cap = instance.Machines[request.machineID-1].size * request.amount
             current_route = trucksUsed[currentTruck].route
-            new_route = insert_in_route(request.locID,trucksUsed[currentTruck].route)
+            new_route = functions.insert_in_route(request.locID,trucksUsed[currentTruck].route)
 
             # Constraint 1: Item should fit
             # Constraint 2: Car should have enough gas to take route
@@ -73,7 +33,7 @@ def solve(instance):
             # Allocate to either current or new truck
             if (
                 ((current_cap + machine_cap) < instance.TruckCapacity) and
-                (calc_route_distance(new_route) <= instance.TruckMaxDistance) and
+                (functions.calc_route_distance(new_route,instance.Locations) <= instance.TruckMaxDistance) and
                 (request.firstDay < trucksUsed[currentTruck].getFirstEndDay())
                 ):
                 trucksUsed[currentTruck].machines.append(instance.Machines[request.machineID - 1])
@@ -87,7 +47,7 @@ def solve(instance):
                 currentTruck += 1
                 trucksUsed[currentTruck].machines.append(instance.Machines[request.machineID - 1])
                 trucksUsed[currentTruck].requests.append(request)
-                trucksUsed[currentTruck].route = insert_in_route(request.locID,trucksUsed[currentTruck].route)
+                trucksUsed[currentTruck].route = functions.insert_in_route(request.locID,trucksUsed[currentTruck].route)
                 day.TruckRequests.append(request)
             requests_in_trucks.append(request)
             satisfied = 0
@@ -103,8 +63,6 @@ def solve(instance):
 
     # End result: solution.Days now contains solutionday objects which contain the truck objects which contain their
     # routes and the request objects that they will serve
-
-
 
     # --- TECHNICIANS --- #
     jobs_next_day = []
@@ -126,22 +84,17 @@ def solve(instance):
                 continue
             for technician in instance.Technicians:
                 current_jobz = [req.ID for req in technician.requests.get(day.dayNumber)]
-                current_diz = calc_route_distance(technician.routes.get(day.dayNumber))
-                satisfied = 0
+                current_diz = functions.calc_route_distance(technician.routes.get(day.dayNumber),instance.Locations)
                 # Constraint 1: Technicians should be available:
-                if technician.available:
-                    satisfied += 1
                 # Constraint 2: Technician should be able to fix machine
-                if job.machineID in technician.machines:
-                    satisfied += 1
                 # Constraint 3: Technician should be able to travel
-                new_route = insert_in_route(job.locID, technician.routes.get(day.dayNumber))
-                if calc_route_distance(new_route) <= technician.maxDistance:
-                    satisfied += 1
                 # Constraint 4: Technician should be able to install
-                if technician.installs < technician.maxInstalls:
-                    satisfied += 1
-                if satisfied == 4:
+                new_route = functions.insert_in_route(job.locID, technician.routes.get(day.dayNumber))
+                if (technician.available and 
+                    job.machineID in technician.machines and
+                    functions.calc_route_distance(new_route,instance.Locations) <= technician.maxDistance and
+                    technician.installs < technician.maxInstalls
+                ):
                     technician.requests.get(day.dayNumber).append(job)
                     technician.routes[day.dayNumber] = new_route
                     technician.installs += 1
@@ -164,23 +117,3 @@ def solve(instance):
 
     # --- Final check --- #
     return solution
-    '''   class SolutionDay(object):
-            def __init__(self, dayNr):
-                self.dayNumber = dayNr
-                self.NrTruckRoutes = None
-                self.TruckRoutes = []
-                self.NrTechnicianRoutes = None
-                self.TechnicianRoutes = []
-                self.TruckRequests = []'''
-
-
-
-        # Loop through requests again to allocate technicians
-
-    '''   self.ID = ID
-            self.locID = locID
-            self.firstDay = firstDay
-            self.LastDay = LastDay
-            self.machineID = machineID
-            self.amount = amount
-    '''
