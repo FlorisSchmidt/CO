f27385525f28fb0242a39902fa86f4fb508d6bbf
floris.schmidt <floris.schmidt@hotmail.com>
2021-05-01 11:06:54 +0200

    Sets, params complete

--- milp.py
+++ milp.py
@@ -2,17 +2,18 @@ import gurobipy as gp
 import classes
 import functions
 import scipy.spatial.distance as distance
+from gurobipy import GRB, quicksum
 
 def solve(instance):
     solution = classes.Solution(instance.Days)
 
     model = gp.Model()
 
-    trucks = [] # max number of trucks required
-
     #Sets
-    R = instance.Requests
-    
+    R = instance.Requests #requests
+    K = 0 # max number of trucks required
+    S = 0 #technicians
+    R_s = []
     #Params
     T = instance.Days
     H_0 = instance.Locations.get(1)
@@ -45,53 +46,74 @@ def solve(instance):
         c_i.append(request.size)
         idle_cost = request.amount*instance.Machines[request.machineID-1].penalty
         CI_i.append(idle_cost)
+        K+=request.amount
 
     for technician in instance.Technicians:
         current_tech = []
+        can_install = [instance.Locations[(technician.locID)]]
         for request in instance.Requests:
             if(request.machineID in technician.machines):
                 current_tech.append(1)
+                can_install.append(request.ID)
             else:
                 current_tech.append(0)
+        
+        R_s.append(can_install)
         a_si.append(current_tech)
         H_s.append(instance.Locations[(technician.locID)])
         D_s.append(technician.maxDistance)
         N_s.append(technician.maxInstalls)
-    
+        S+=1
 
     #Decision variables
     #x_ij_k truck visits location j after location i: 1 if truck visits, 0 otherwise
     #LIST MET LOCATIONS NOG INVULLEN
-    x = model.addVar((), vtype=GRB.BINARY, name='xij')
 
+    x = model.addVars(((t,k,i,j) for t in range(T) for k in range(K) for i in R for j in R), vtype=GRB.BINARY, name='xij')
+
+    z = model.addVars(((t,s,i,j) for t in range(T) for s in range(S) for i in R for j in R), vtype=GRB.BINARY, name='zij')
     #m_k number of trucks used for entire planning: 1 if truck k is used, 0 otherwise
-    m = model.addVar((k for k in trucks), ub=D , vtype=GRB.BINARY, name = 'numTrucks')
+    m = model.addVars((k for k in range(K)), vtype=GRB.BINARY, name = 'numTrucks')
 
     #r_s number of technicians used for entire planning: 1 if technician is used, 0 otherwise
-    r = model.addVar((s for s in instance.Technicians), vtype=GRB.BINARY, name = 'r')
+    r = model.addVars((s for s in range(S)), vtype=GRB.BINARY, name = 'r')
 
     #v_k_t truck k is used during day: 1 if used, 0 otherwise
-    v = model.addVar(((day,k) for day in instance.Days for k in trucks), vtype=GRB.BINARY, name = 'vkt')
+    v = model.addVars(((t,k) for t in range(T) for k in range(K)), vtype=GRB.BINARY, name = 'vkt')
 
     #p_s_t technician s is used during day: 1 if used, 0 otherwise
-    p = model.addVar(((day,s) for day in instance.Days for s in instance.Technicians), vtype=GRB.BINARY, name = 'pst')
+    p = model.addVars(((t,s) for t in range(T) for s in range(S)), vtype=GRB.BINARY, name = 'pst')
 
     #w_i_t request i is delivered on day: 1 if delivered, 0 otherwise
-    w = model.addVar(((day,t) for day in instance.Days for t in request), vtype=GRB.BINARY, name = 'wit')
+    w = model.addVars(((t,i) for t in range(T) for i in R), vtype=GRB.BINARY, name = 'wit')
 
     #y_i_t request i is installed on day: 1 if installed, 0 otherwise
-    #y = model.addVar(((day,i) for day in instance.Days for i in instance.installation), vtype=GRB.BINARY, name = 'yit')
+    y = model.addVars(((t,i) for t in range(T) for i in R), vtype=GRB.BINARY, name = 'yit')
 
     #g_is number of visits done by technician
-    #g = model.addVar((route, request) for route in SolutionDay.TechnicianRoute for request in SolutionDay.TechnicianRequests,vtype=GRB.INTEGER, name='gis')
+    g = model.addVars(((s, i) for s in range(S) for i in R),vtype=GRB.INTEGER, name='gis')
 
     #b_i number of days installation of request j is delayed after its delivery
+    b = model.addVars(((i) for i in R),vtype=GRB.INTEGER, name='b')
+
+    #Objective
+    model.setObjective(quicksum(m),GRB.MINIMIZE)
+    print((v[0,0]))
+    print(m[0])
+    model.addConstrs((v[t,k] <= m[k]) for k in range(K) for t in range(T))
+
+    model.Params.LogToConsole = True
+    model.optimize()
+    model.update()
+    print("End")
+    #def cost():
+    #    quicksum(CV*m)# + quicksum(CV*i for i in v) + quicksum(quicksum())
 
     #Constraints
     #max distance truck constraint
-    model.addConstr((gp.quicksum(d_ij[i][j]*x[i,j,k] for i,j in coordinates) <= D for k in trucks), name='maxTruckDistance')
+    #model.addConstr((quicksum(d_ij[i][j]*x[i,j,k] for i,j in coordinates) <= D for k in trucks), name='maxTruckDistance')
 
     #ensures that no more trucks can be used on a day than the number of trucks for the entire planning
-    model.addConstr((v[day,k] for day in instance.Days <= m[k] for k in trucks), name='noMoreThanMaxTrucksOnADay')
+    #model.addConstr((v[day,k] for day in instance.Days <= m[k] for k in trucks), name='noMoreThanMaxTrucksOnADay')
 
     
